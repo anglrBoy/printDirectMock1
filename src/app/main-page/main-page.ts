7df53016f26bdc2b0e6bccc34ebe5e64b69528ed
frozenBug <feby.denahon@gmail.com>
2026-06-06 20:02:04 -0700

    Screen support
    Support for different screens

--- src/app/main-page/main-page.ts
+++ src/app/main-page/main-page.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.html',
   styleUrl: './main-page.css'
 })
-export class MainPage {}
+export class MainPage {
+  windowWidth = window.innerWidth;
+
+  @HostListener('window:resize')
+  onResize() {
+    this.windowWidth = window.innerWidth;
+  }
+}
